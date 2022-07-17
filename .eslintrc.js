3b3df2d8bd18912da4c7a2b6b97514dcb038bf52
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-07-17 16:42:18 +0900

    :tada: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-console": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# @KoichiKiyokawa/eslint-config-prettier-react-typescript
+
+## How to use
+### 1. Install
+
+npm
+```bash
+npm i -D https://github.com/KoichiKiyokawa/eslint-config-prettier-react-typescript#v0.0.1
+```
+
+or yarn
+```bash
+yarn add -D https://github.com/KoichiKiyokawa/eslint-config-prettier-react-typescript#v0.0.1
+```
+
+or pnpm
+```bash
+pnpm i -D https://github.com/KoichiKiyokawa/eslint-config-prettier-react-typescript#v0.0.1
+```
+
+### 2. Create `.eslintrc.yml`
+```yml
+extends: @KoichiKiyokawa/eslint-config-prettier-react-typescript
+```
+
+### 3. Set npm script
+`package.json`
+```json
+"scripts": {
+  "lint": "eslint 'src/**/*.{ts,tsx}' --ignore-path .gitignore && prettier --write . --ignore-path .gitignore"
+}
+```
